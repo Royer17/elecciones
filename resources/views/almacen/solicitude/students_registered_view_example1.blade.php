d92c3cff31151b2eebaa46c607ec458b91f36908
Luis <luis.charres.06@gmail.com>
2025-10-31 13:21:28 -0500

    improve students list

--- resources/views/almacen/solicitude/students_registered_view_example1.blade.php
+++ resources/views/almacen/solicitude/students_registered_view_example1.blade.php
@@ -36,11 +36,11 @@
 
                     <!-- Filtros y búsqueda -->
                     <div class="card mb-4">
-                        <div class="card-header py-2">
+                        <!-- <div class="card-header py-2">
                             <h4 class="card-title">
                                 <i class="fa fa-filter"></i> Filtros de Búsqueda
                             </h4>
-                        </div>
+                        </div> -->
                         <div class="card-body py-2">
                             <div class="row align-items-end">
                                 <div class="col-6">
@@ -64,7 +64,6 @@
                                     {{ Form::close() }}
                                 </div>
 
-
                                 <div class="col-auto">
                                     <div class="form-group mb-0">
                                         <label class="control-label">Importar desde Excel:</label>
@@ -77,104 +76,112 @@
                                     <a href="/plantilla_importar_estudiantes.xlsx" class="btn btn-secondary"
                                         target="_blank"><i class="fa fa-download"></i> Plantilla</a>
                                 </div>
+
+                                <div class="col-auto">
+                                    <a href="/admin/registrar-estudiante" class="btn btn-md btn-success"><i
+                                            class="fa fa-plus-circle"></i> Nuevo estudiante</a>
+                                </div>
+
                             </div>
                         </div>
+                    </div>
+                    <!-- Fin de filtros y búsqueda -->
 
-                        <!-- Tabla de estudiantes -->
-                        <div class="table-responsive">
-                            <table class="table table-striped table-bordered table-condensed table-hover table-sm">
-                                <thead class="thead-dark">
+
+                    <!-- Tabla de estudiantes -->
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-condensed table-hover table-sm">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th class="text-center">#</th>
+                                    <th>Código</th>
+                                    <th>Fecha de creación</th>
+                                    <th>DNI</th>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Nivel</th>
+                                    <th class="text-center">Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($orders as $key => $cat)
                                     <tr>
-                                        <th class="text-center">#</th>
-                                        <th>Código</th>
-                                        <th>Fecha de creación</th>
-                                        <th>DNI</th>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>Nivel</th>
-                                        <th class="text-center">Opciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($orders as $key => $cat)
-                                        <tr>
-                                            <td class="text-center">{{ $key+1 }}</td>
-                                            <td>{{ $cat->code }}</td>
-                                            <td>{{ \Date::parse($cat->created_at)->format('d/F/Y H:i') }}
-                                            </td>
-                                            <td>{{ $cat->identity_document }}</td>
-                                            <td>{{ $cat->name }}</td>
-                                            <td>{{ $cat->paternal_surname }}</td>
-                                            @if($cat->order_type_id == 1)
-                                                <td>Primaria</td>
-                                            @elseif($cat->order_type_id == 2)
-                                                <td>Secundaria</td>
-                                            @else
-                                                <td class="text-danger">Error</td>
-                                            @endif
-                                            <td>
-                                                <div class="dropdown export_matriculas">
-                                                    <button type="button" class="btn btn_state dropdown-toggle"
-                                                        data-toggle="dropdown">Acción</button>
-                                                    <div class="dropdown-menu">
-                                                        <a class="dropdown-item" data-i="{{ $cat->id }}"
-                                                            data-index="3" data-action_text="Retirar"
-                                                            href="javascript:void(0);"
-                                                            onclick="changeStatus(this)">Retirar</a>
-                                                    </div>
+                                        <td class="text-center">{{ $key+1 }}</td>
+                                        <td>{{ $cat->code }}</td>
+                                        <td>{{ \Date::parse($cat->created_at)->format('d/F/Y H:i') }}
+                                        </td>
+                                        <td>{{ $cat->identity_document }}</td>
+                                        <td>{{ $cat->name }}</td>
+                                        <td>{{ $cat->paternal_surname }}</td>
+                                        @if($cat->order_type_id == 1)
+                                            <td>Primaria</td>
+                                        @elseif($cat->order_type_id == 2)
+                                            <td>Secundaria</td>
+                                        @else
+                                            <td class="text-danger">Error</td>
+                                        @endif
+                                        <td>
+                                            <div class="dropdown export_matriculas">
+                                                <button type="button" class="btn btn_state dropdown-toggle"
+                                                    data-toggle="dropdown">Acción</button>
+                                                <div class="dropdown-menu">
+                                                    <a class="dropdown-item" data-i="{{ $cat->id }}" data-index="3"
+                                                        data-action_text="Retirar" href="javascript:void(0);"
+                                                        onclick="changeStatus(this)">Retirar</a>
                                                 </div>
+                                            </div>
 
-                                            </td>
-                                        </tr>
-                                        @include('almacen.solicitude.entity')
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                        </td>
+                                    </tr>
+                                    @include('almacen.solicitude.entity')
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
-                        <!-- Paginación -->
-                        <div class="text-center mt-3">
-                            {{ $orders->appends(request()->input())->render() }}
-                        </div>
+                    <!-- Paginación -->
+                    <div class="text-center mt-3">
+                        {{ $orders->appends(request()->input())->render() }}
                     </div>
                 </div>
             </div>
-            @include('almacen.solicitude.modal')
+        </div>
+        @include('almacen.solicitude.modal')
 
-            @push('scripts')
-                <script>
-                    $(document).ready(function () {
-                        $('#liGeneracionInterna').addClass("treeview active");
-                        $('#liEnrollment').addClass("active");
-                    });
+        @push('scripts')
+            <script>
+                $(document).ready(function () {
+                    $('#liGeneracionInterna').addClass("treeview active");
+                    $('#liEnrollment').addClass("active");
+                });
 
-                    // Funcionalidad para importar estudiantes
-                    $(`#student__import`).on('click', function (e) {
-                        e.preventDefault();
+                // Funcionalidad para importar estudiantes
+                $(`#student__import`).on('click', function (e) {
+                    e.preventDefault();
 
-                        if (!data_imports) {
-                            notice("Advertencia", "No se han seleccionado datos para importar", `warning`);
-                            return;
-                        }
+                    if (!data_imports) {
+                        notice("Advertencia", "No se han seleccionado datos para importar", `warning`);
+                        return;
+                    }
 
-                        lockWindow();
-                        axios.post(`/admin/students/importv2`, {
-                                nivel: nivel,
-                                identity_document: identity_document,
-                                surname: surname,
-                                name: name
-                            })
-                            .then((response) => {
-                                unlockWindow();
-                                notice(response.data.title, response.data.message, `success`);
-                                location.reload();
-                            })
-                            .catch((error) => {
-                                unlockWindow();
-                                notice(error.response.data.title, error.response.data.message,
-                                    `warning`);
-                            });
-                    });
-                </script>
-            @endpush
-            @endsection
+                    lockWindow();
+                    axios.post(`/admin/students/importv2`, {
+                            nivel: nivel,
+                            identity_document: identity_document,
+                            surname: surname,
+                            name: name
+                        })
+                        .then((response) => {
+                            unlockWindow();
+                            notice(response.data.title, response.data.message, `success`);
+                            location.reload();
+                        })
+                        .catch((error) => {
+                            unlockWindow();
+                            notice(error.response.data.title, error.response.data.message,
+                                `warning`);
+                        });
+                });
+            </script>
+        @endpush
+        @endsection
